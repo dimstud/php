5752afd8e3fb02b02599f917ddc7913a8d84d6c8
Dmitry Ageev <dimichageev@gmail.com>
2023-11-21 14:24:10 +0300

    Работа с файлами завершена

--- includ/includes/files.php
+++ includ/includes/files.php
@@ -5,12 +5,52 @@
         move_uploaded_file($_FILES['file']['tmp_name'],'uploads/'.$_FILES['file']['name']);
     }
 
-    if (file_exists('uploads/dc.png')) {
-        echo "dc здесь!";
-    }
+    // if (is_file('uploads/dc1.png')) {
+    //     echo "dc есть!";
+    // }
+    // if (is_dir('uploads')) {
+    //     echo "uploads есть!";
+    // }
 
-    rmdir('test');
-    rename('uploads/dc.png', 'uploads/dc1.png');
+    // rmdir('test');
+    // rename('uploads/dc.png', 'uploads/dc1.png');
+
+    // $text = file_get_contents('https://povar-ru.turbopages.org/povar.ru/s/recipes/pshennaya_kasha_v_multivarke-11285.html');
+    // echo htmlspecialchars($text);
+
+    // $text = file('https://povar-ru.turbopages.org/povar.ru/s/recipes/pshennaya_kasha_v_multivarke-11285.html');
+    
+    // foreach($text as $num => $line){
+    //     echo $num . ": " . htmlspecialchars($line) . "<br>" . "<br>";
+    // }
+
+    // $file = 'uploads/text.txt';
+    // $var = "Hello\n";
+
+    // $b = file_put_contents($file, $var, FILE_APPEND | LOCK_EX);
+
+    // echo $b;
+
+
+
+    // $file = 'uploads/text.txt';
+    // echo filesize($file);
+
+
+
+
+    $file = 'uploads/text.txt';
+
+    $handle = fopen($file, 'a+');
+
+    $text = fread($handle, filesize($file));
+
+    fwrite($handle, 'HHHHaaahhah');
+
+    fclose($handle);
+
+    echo $text;
+
 ?>
 
 <form method="POST" action="" enctype="multipart/form-data">
