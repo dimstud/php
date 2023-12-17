151334b97f511edbb0d10f3c61fbf527f68e2da4
Dmitry Ageev <dimanagee@yandex.ru>
2023-12-17 11:39:24 +0300

    b6

--- book/index.php
+++ book/index.php
@@ -1,4 +1,14 @@
 <?php
-$arr=['as'=>1];
-echo $arr['as']
+$arr=array(
+    'Alex'=>1,
+    'Dima'=>3,
+    'Kris'=>4,
+    'bell'=>array(
+        'are'=>34,
+        'sex'=>'women'
+    )
+    );
+var_dump($arr);
+sort($arr, SORT_NUMERIC);
+var_dump($arr);
 ?>
